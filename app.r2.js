bb65ae86dda2681ca301b8f4a9804ac475c9bb70
Andre Paiva <andrepaiva.arq@gmail.com>
2020-09-06 23:31:51 -0300

    animated

--- app.r2.js
+++ app.r2.js
@@ -1,6 +1,5 @@
 import { GLTFLoader } from './tools/GLTFLoader.js';
 import { OrbitControls } from './tools/OrbitControls.js';
-import { AnimationMixer } from './tools/three.module.js';
 
 const renderer = new THREE.WebGLRenderer({
   antialias:true,
@@ -10,10 +9,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.querySelector("#viewport").appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-//scene.background = new THREE.Color(0xdddddd);
 
-const ambientLight = new THREE.AmbientLight (0x404040,100);
+const ambientLight = new THREE.AmbientLight (0x404040,5);
 scene.add(ambientLight);
+const directLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+directLight.position.set(.2,.1,0);
+scene.add(directLight);
 
 const camera = new THREE.PerspectiveCamera(75,
   window.innerWidth/window.innerHeight,
@@ -23,38 +24,34 @@ const camera = new THREE.PerspectiveCamera(75,
 const controls = new OrbitControls (camera, renderer.domElement);
 camera.position.set (0,0,8);
 
+var mixer, magnetField;
 const loader = new GLTFLoader();
 loader.load('./assets/modelo.glb',
   function (gltf){
-    var modelo = gltf.scene;
+    var magnets = gltf.scene.children[0];
+    magnetField = gltf.scene.children[1];
+    scene.add(magnets);
+    scene.add(magnetField);
 
-
-
-    scene.add(modelo);
+    mixer = new THREE.AnimationMixer(magnetField);
+    var action = mixer.clipAction(gltf.animations[0]);
+    action.play();
   }),
   undefined,
   function(error){
     console.error(error);
   }
 
-animate();
-render();
-
+var clock = new THREE.Clock();
+renderer.setAnimationLoop(render);
+requestAnimationFrame(render);
+  
 function render() {
-  requestAnimationFrame(render);
   renderer.render(scene, camera);
-}
-
-function animate() {
-
-  requestAnimationFrame( animate );
-  controls.update();
-	renderer.render( scene, camera );
-
+  if (mixer) mixer.update(clock.getDelta());
 }
 
 window.addEventListener('resize', onResize);
-
 function onResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
